37952dc4ed8957b61cf7b4cf4b8144889f48b887
smw0807 <smw0807@gmail.com>
2024-10-24 22:45:46 +0900

    supabase 적용

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { validationSchema } from './validation.schema';
 import commonConfig from './conf/common.config';
 import corsConfig from './conf/cors.config';
+import supabaseConfig from './conf/supabase.config';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-      load: [commonConfig, corsConfig],
+      load: [commonConfig, corsConfig, supabaseConfig],
       isGlobal: true,
       validationSchema,
     }),
