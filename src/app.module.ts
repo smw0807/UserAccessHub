815e96c58531abe4d9af719ae8bf563a6d3b7895
smw0807 <smw0807@gmail.com>
2024-10-24 22:47:20 +0900

    supabase 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, SupabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
