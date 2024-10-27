b6dac426589ff4b16362891fc6425317c098f802
smw0807 <smw0807@gmail.com>
2024-10-27 10:41:44 +0900

    test

--- src/graphql/graphql.resolver.ts
+++ src/graphql/graphql.resolver.ts
@@ -6,9 +6,10 @@ export class GraphqlResolver {
   private readonly logger = new Logger(GraphqlResolver.name);
   constructor() {}
 
-  @Query(() => String, { name: 'hello' })
-  async hello() {
+  @Query(() => String)
+  async helloTest() {
     this.logger.log('hello resolver');
     return 'Hello GraphQL';
+    // throw new Error('Hello GraphQL');
   }
 }
