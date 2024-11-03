8ed427c70624f515341616ce068495ffb49d138a
smw0807 <smw0807@gmail.com>
2024-11-03 16:07:58 +0900

    구글, 카카오 로그인 엔드포인트 마무리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Logger,
-  Param,
-  Query,
-  Req,
-  Res,
-} from '@nestjs/common';
+import { Controller, Get, Logger, Query, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { AuthGoogleService } from './auth.google.service';
@@ -38,23 +30,35 @@ export class AuthController {
   @Get('callback/google')
   async callbackGoogle(@Query('code') code: string, @Res() res: Response) {
     try {
-      const data = await this.authGoogleService.getGoogleUser(code);
+      // 구글 로그인 인증 토큰 발급
+      const tokens = await this.authGoogleService.getGoogleAuthToken(code);
+      // 구글 로그인 유저 정보 조회
+      const userData = await this.authGoogleService.getGoogleUser(
+        tokens.access_token,
+      );
+
       // 가입된 이메일 있는지 확인
-      let user = await this.userService.findUserByEmail(data.email);
+      let user = await this.userService.findUserByEmail(userData.email);
       if (!user) {
         // 소셜 로그인 회원 가입
         const socialUser = await this.userService.addSocialUser({
-          email: data.email,
-          name: data.name,
+          email: userData.email,
+          name: userData.name,
           type: SIGN_UP_TYPE.GOOGLE,
-          profileImage: data.picture,
+          profileImage: userData.picture,
         });
         this.logger.log(`소셜 로그인 회원 가입 성공: ${socialUser.email}`);
-        user = await this.userService.findUserByEmail(data.email);
+        user = await this.userService.findUserByEmail(userData.email);
       }
-      const tokens = await this.authService.makeTokens(user);
+      // 토큰 생성
+      const tokenInfo = await this.authService.makeTokens(user, {
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token,
+        token_type: tokens.token_type,
+        expiry_date: tokens.expiry_date,
+      });
 
-      return res.status(200).send(tokens);
+      return res.status(200).send(tokenInfo);
     } catch (e) {
       console.log('test : ', e.message);
       this.logger.error(e);
@@ -73,13 +77,40 @@ export class AuthController {
     }
   }
 
-  @Get('callback/:provider')
-  async callback(
-    @Param('provider') provider: string,
-    @Req() req: Request,
-    @Res() res: Response,
-  ) {
-    console.log(typeof provider);
-    return res.status(200).send('callback');
+  @Get('callback/kakao')
+  async callbackKakao(@Query('code') code: string, @Res() res: Response) {
+    try {
+      // 카카오 로그인 인증 토큰 발급
+      const tokens = await this.authKakaoService.getKakaoAuthToken(code);
+      // 카카오 로그인 유저 정보 조회
+      const userData = await this.authKakaoService.getKakaoUser(
+        tokens.access_token,
+        tokens.token_type,
+      );
+      // 가입된 이메일 있는지 확인
+      let user = await this.userService.findUserByEmail(
+        userData.kakao_account.email,
+      );
+      if (!user) {
+        // 소셜 로그인 회원 가입
+        const socialUser = await this.userService.addSocialUser({
+          email: userData.kakao_account.email,
+          name: userData.properties.nickname,
+          type: SIGN_UP_TYPE.KAKAO,
+          profileImage: userData.properties.profile_image,
+        });
+        this.logger.log(`소셜 로그인 회원 가입 성공: ${socialUser.email}`);
+        user = await this.userService.findUserByEmail(
+          userData.kakao_account.email,
+        );
+      }
+      // 토큰 생성
+      const tokenInfo = await this.authService.makeTokens(user, tokens);
+
+      return res.status(200).send(tokenInfo);
+    } catch (e) {
+      this.logger.error(e);
+      return res.status(500).send(e.message);
+    }
   }
 }
