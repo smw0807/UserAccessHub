afd2e991c4c0a131885d2b49c2a7e5250fe05939
smw0807 <smw0807@gmail.com>
2024-11-03 14:33:43 +0900

    카카오 로그인 토큰 발급 로직 추가

--- src/auth/auth.kakao.service.ts
+++ src/auth/auth.kakao.service.ts
@@ -5,15 +5,40 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthKakaoService {
   private readonly logger = new Logger(AuthKakaoService.name);
+  private restApiKey: string;
+  private redirectUri: string;
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    this.restApiKey = this.configService.get('kakao.restApiKey');
+    this.redirectUri = this.configService.get('kakao.redirectUri');
+  }
 
+  // 카카오 로그인 URL 생성
   getKakaoAuthUrl() {
-    const resApikey = this.configService.get('kakao.restApiKey');
-    const redirectUrl = this.configService.get('kakao.redirectUri');
-    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${resApikey}&redirect_uri=${redirectUrl}&response_type=code&social=kakao`;
-    return url;
+    return `https://kauth.kakao.com/oauth/authorize?client_id=${this.restApiKey}&redirect_uri=${this.redirectUri}&response_type=code&state=kakao`;
+  }
+
+  // 카카오 로그인 인증 토큰 발급
+  async getKakaoAuthToken(code: string) {
+    try {
+      const url = 'https://kauth.kakao.com/oauth/token';
+      const data = {
+        grant_type: 'authorization_code',
+        client_id: this.restApiKey,
+        code,
+      };
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+      const response = await this.httpService.axiosRef.post(url, data, {
+        headers,
+      });
+      return response.data;
+    } catch (e) {
+      this.logger.error('getKakaoUser', e);
+      throw new Error(e);
+    }
   }
 }
