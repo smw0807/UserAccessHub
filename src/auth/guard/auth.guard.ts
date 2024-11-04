8e7b0b1921850fabd6e85140a38cb4d54985313f
smw0807 <smw0807@gmail.com>
2024-11-04 20:49:52 +0900

    AuthGuard 추가

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,28 @@
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+
+import { Injectable } from '@nestjs/common';
+
+import { CanActivate } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const ctx = GqlExecutionContext.create(context);
+      const headers = ctx.getContext().req.headers;
+      const authHeader = headers.authorization;
+      const token = authHeader.split(' ')[1];
+
+      const user = await this.authService.verifyToken(token);
+      ctx.getContext().req.user = user;
+
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
