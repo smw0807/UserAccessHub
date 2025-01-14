188187b319bfc0eded6ce92867b597ad15a5a0d3
smw0807 <smw0807@gmail.com>
2025-01-14 11:41:46 +0900

    포인트 적립로직 함수 분리

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -6,40 +6,97 @@ export class PointService {
   private readonly logger = new Logger(PointService.name);
   constructor(private readonly prisma: PrismaService) {}
 
+  /**
+   * 적립금 생성
+   * @param userId
+   * @param point
+   * @param reason
+   */
   async createPoint(userId: string, point: number, reason?: string) {
     try {
-      const pointOrigin = await this.prisma.point.create({
-        data: {
-          point: point,
-          reason: reason,
-          user: {
-            connect: {
-              id: userId,
-            },
-          },
+      // userId 로 생성된 포인트 데이터가 있는지 확인 (있으면 update, 없으면 create)
+      const pointOrigin = await this.prisma.point.findFirst({
+        where: {
+          userId,
         },
       });
-      this.logger.log(`적립금 생성 성공: ${pointOrigin.id}`);
-      const pointHistory = await this.prisma.pointHistory.create({
-        data: {
-          point: point,
-          reason: reason,
-          pointOrigin: {
-            connect: {
-              id: pointOrigin.id,
+      if (pointOrigin) {
+        await this.updatePoint(
+          pointOrigin.id,
+          userId,
+          pointOrigin.point + point,
+          reason,
+        );
+      } else {
+        const pointOrigin = await this.prisma.point.create({
+          data: {
+            point: point,
+            reason: reason,
+            user: {
+              connect: {
+                id: userId,
+              },
             },
           },
-          user: {
-            connect: {
-              id: userId,
-            },
-          },
-        },
-      });
-      this.logger.log(`적립금 히스토리 생성 성공: ${pointHistory.id}`);
+        });
+        this.logger.log(`적립금 생성 성공: ${pointOrigin.id}`);
+      }
+      await this.createPointHistory(pointOrigin.id, userId, point, reason);
     } catch (e) {
       this.logger.error(e);
       throw new Error(e);
     }
+  }
+
+  /**
+   * 적립금 업데이트
+   * @param pointOriginId
+   * @param userId
+   * @param point
+   * @param reason
+   */
+  async updatePoint(
+    pointOriginId: string,
+    userId: string,
+    point: number,
+    reason?: string,
+  ) {
+    await this.prisma.point.update({
+      where: { userId },
+      data: { point: point, reason: reason },
+    });
+    this.logger.log(`적립금 업데이트 성공: ${pointOriginId}`);
+  }
+
+  /**
+   * 적립금 히스토리 생성
+   * @param pointOriginId
+   * @param userId
+   * @param point
+   * @param reason
+   */
+  async createPointHistory(
+    pointOriginId: string,
+    userId: string,
+    point: number,
+    reason?: string,
+  ) {
+    const pointHistory = await this.prisma.pointHistory.create({
+      data: {
+        point: point,
+        reason: reason,
+        pointOrigin: {
+          connect: {
+            id: pointOriginId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+    this.logger.log(`적립금 히스토리 생성 성공: ${pointHistory.id}`);
   }
 }
