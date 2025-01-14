1267aaba3cacf62891179b7534224de9653356c9
smw0807 <smw0807@gmail.com>
2025-01-14 12:03:18 +0900

    적립금 모델 수정

--- src/point/model/point.model.ts
+++ src/point/model/point.model.ts
@@ -8,6 +8,15 @@ export class PointModel {
   @Field({ description: '적립금 포인트', defaultValue: 0 })
   point: number;
 
+  @Field({ description: '적립 이유', defaultValue: '' })
+  reason: string;
+
+  @Field({ nullable: true, description: '적립금 생성 시간' })
+  createdAt?: Date;
+
+  @Field({ nullable: true, description: '적립금 수정 시간' })
+  updatedAt?: Date;
+
   @Field(() => [PointHistoryModel], {
     nullable: true,
     description: '적립금 히스토리',
@@ -25,4 +34,10 @@ export class PointHistoryModel {
 
   @Field({ description: '적립금 히스토리 이유' })
   reason: string;
+
+  @Field({ nullable: true, description: '적립금 히스토리 생성 시간' })
+  createdAt?: Date;
+
+  @Field({ nullable: true, description: '적립금 히스토리 수정 시간' })
+  updatedAt?: Date;
 }
