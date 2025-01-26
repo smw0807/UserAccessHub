bc8a03928457040d6ff9fc4590ed4c057d61b50b
smw0807 <smw0807@gmail.com>
2025-01-26 13:02:48 +0900

    string -> number

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -14,7 +14,26 @@ export class PointController {
     @Res() res: Response,
   ) {
     try {
-      const result = await this.pointService.getPointHistoryList(input);
+      const { keyword, page, size } = input;
+      if (!page || !size) {
+        return res.status(400).send({
+          success: false,
+          message: '페이지 번호와 페이지 크기는 필수 입니다.',
+        });
+      }
+      let pageNumber = page;
+      let pageSize = size;
+      if (typeof page === 'string') {
+        pageNumber = parseInt(page);
+      }
+      if (typeof size === 'string') {
+        pageSize = parseInt(size);
+      }
+      const result = await this.pointService.getPointHistoryList({
+        keyword,
+        page: pageNumber,
+        size: pageSize,
+      });
       return res.send({
         success: true,
         message: '적립금 히스토리 목록 조회 성공',
--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -3,10 +3,12 @@ import { PointService } from './point.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PointResolver } from './point.resolver';
 import { AuthModule } from 'src/auth/auth.module';
+import { PointController } from './point.controller';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => AuthModule)],
   providers: [PointService, PointResolver],
   exports: [PointService],
+  controllers: [PointController],
 })
 export class PointModule {}
